1fa5d45ebeef0fbfc8ce9d270df23e5544439d5c
evanpaul22 <evanpaul22@gmail.com>
2017-05-28 21:49:28 -0700

    Clean old files

--- event.py
+++
@@ -1,9 +0,0 @@
-class Node:
-    knockout_refs = []
-    killer_ref = None
-
-    def __init__(self, victim, villain, weapon=None, kill_flag=True):
-        self.name = victim
-        self.killer_name = villain
-        self.weapon = weapon
-        self.kill_flag = kill_flag
